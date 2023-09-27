7bfe8fb1469f11193903a2330fd9a786d1e6cdd4
anaskhan3458027 <anaskhan3458027@gmail.com>
2023-09-27 23:47:05 +0530

    add

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,8 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+int binary_search(int arr[], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+    int mid = (start + end) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+
+        mid = (start + end) / 2;
+    }
+    return -1;
+}
 int main()
 {
-    vector<int>v;
-     
- return 0;
+    int n, key, arr[10];
+    cout << "enter array length :";
+    cin >> n;
+    cout << "enter array:";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "enter key:";
+    cin >> key;
+    cout<<binary_search(arr, n, key);
 }
